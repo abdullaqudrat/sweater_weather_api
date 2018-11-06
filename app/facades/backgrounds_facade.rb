f6c329a3a76dcf17bca16268c91fd663a3434c70
abdullaqudrat <aqudrat630@gmail,com>
2018-11-06 15:43:12 -0700

    moves facades to facade folder

--- app/facades/backgrounds_facade.rb
+++ app/facades/backgrounds_facade.rb
@@ -7,6 +7,6 @@ class BackgroundsFacade
 
   def retrieve_image_links
     geocode_data = GeocodeService.new(@location).formatted_address_and_coordinates
-    BackgroundImageService.new(geocode_data).get_backgrounds
+    BackgroundSerializer.new(BackgroundImageService.new(geocode_data).get_backgrounds).serializable_hash
   end
 end
