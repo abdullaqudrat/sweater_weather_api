1a475edca31c7f981739def9114eae8805a4b5ee
abdullaqudrat <aqudrat630@gmail,com>
2018-11-06 15:45:40 -0700

    Revert "adds route/controller for backgrounds api ep"
    This reverts commit cc9c6506a3d0744aeea936b2b510dfb43ee0060b.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
-      get '/backgrounds', to: 'backgrounds#show'
     end
   end
 end
