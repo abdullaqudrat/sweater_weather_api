68c4408f14347cf1f64923717d701964ee4cd805
abdullaqudrat <aqudrat630@gmail,com>
2018-11-07 09:01:32 -0700

    deletes unused code, in favorites#create

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -6,7 +6,6 @@ class Api::V1::FavoritesController < ApplicationController
 
   def create
     location = request.headers['location']
-    # forecast = ForecastFacade.new(location).get_forecast
     favorite = @current_user.favorites.new(location: location)
     if favorite.save
       render json: {location: favorite.location}, status: 201
