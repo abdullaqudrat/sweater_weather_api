db518f40789eac3339dd5dded4e3940a51e51c83
abdullaqudrat <aqudrat630@gmail,com>
2018-11-06 21:06:32 -0700

    adds skip before action to controllers

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BackgroundsController < ApplicationController
+  skip_before_action :authenticate_request
 
   def show
     render json: BackgroundsFacade.new(params[:location]).return_random_image
--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
+  skip_before_action :authenticate_request
 
   def show
     render json: ForecastFacade.new(params[:location]).get_forecast
