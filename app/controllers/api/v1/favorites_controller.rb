b355fdf6a28e3a7067fb64afd55c457f3050400d
abdullaqudrat <aqudrat630@gmail,com>
2018-11-06 21:54:25 -0700

    adds auth commands, updates favorites controller create action

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::FavoritesController < ApplicationController
 
   def create
-    
+    location = request.headers['location']
+    # forecast = ForecastFacade.new(location).get_forecast
+    favorite = @current_user.favorites.new(location: location)
+    if favorite.save
+      render json: {location: favorite.location}, status: 201
+    else
+      render json: {error: 'not favorited'}, status: 400
+    end
   end
 
 end
