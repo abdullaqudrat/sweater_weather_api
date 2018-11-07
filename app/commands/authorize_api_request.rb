23e262aae03ff5d244d102eb007920b98f652a2f
abdullaqudrat <aqudrat630@gmail,com>
2018-11-07 13:36:06 -0700

    Update app/commands/authorize_api_request.rb

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -10,24 +10,21 @@ class AuthorizeApiRequest
   end
 
   private
-
-  attr_reader :headers
-
-  def user
-    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-    @user || errors.add(:token, 'Invalid token') && nil
-  end
-
-  def decoded_auth_token
-    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
-  end
-
-  def http_auth_header
-    if headers['api_key'].present?
-      return headers['api_key'].split(' ').last
-    else
-      errors.add(:token, 'Missing token')
+    def user
+      @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+      @user || errors.add(:token, 'Invalid token') && nil
     end
-    nil
-  end
+
+    def decoded_auth_token
+      @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+    end
+
+    def http_auth_header
+      if @headers['api_key'].present?
+        return @headers['api_key'].split(' ').last
+      else
+        errors.add(:token, 'Missing token')
+      end
+      nil
+    end
 end
