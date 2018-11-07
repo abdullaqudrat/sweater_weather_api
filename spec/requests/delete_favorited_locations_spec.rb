1fe2f1d951e15aee4823df117548059d3a39de81
abdullaqudrat <aqudrat630@gmail,com>
2018-11-07 09:23:57 -0700

    adds sad paths for delete user favorite, changes expectations for happy path

--- spec/requests/delete_favorited_locations_spec.rb
+++ spec/requests/delete_favorited_locations_spec.rb
@@ -13,7 +13,7 @@ describe "delete favorites api" do
     login_response = JSON.parse(response.body)
     login_api_key = login_response['api_key']
 
-    delete "/api/v1/favorites", headers: {'api_key': login_api_key}
+    delete "/api/v1/favorites", headers: {'api_key': login_api_key, 'location': favorite_1.location}
 
     expect(response).to be_successful
 
@@ -25,7 +25,7 @@ describe "delete favorites api" do
     expect(deleted_favorite_response[0]['data']).to have_key('attributes')
     expect(deleted_favorite_response[0]['data']['attributes']).to have_key('location')
     expect(deleted_favorite_response[0]['data']['attributes']['location']).to eq(favorite_2.location)
-    expect(deleted_favorite_response[1]['data']['attributes']['location']).to eq(nil)
+    expect(deleted_favorite_response[1]).to eq(nil)
   end
   it "logged in user cannot delete favorite locations by favorites api end point with NO API KEY" do
     favorite_1 = Favorite.create(location: "Denver, CO, USA")
@@ -42,5 +42,21 @@ describe "delete favorites api" do
     delete "/api/v1/favorites"
 
     expect(response.status).to eq(401)
+  end
+  it "logged in user cannot delete non favorited location by favorites api end point" do
+    favorite_1 = Favorite.create(location: "Denver, CO, USA")
+    favorite_2 = Favorite.create(location: "Los Angeles, CA, USA")
+    user = User.create(email: "whatever@example.com", password: "password", password_confirmation: "password", favorites: [favorite_1])
+
+    post '/api/v1/sessions', headers: {"email": "#{user.email}", "password": "#{user.password}"}
+
+    expect(response).to be_successful
+
+    login_response = JSON.parse(response.body)
+    login_api_key = login_response['api_key']
+
+    delete "/api/v1/favorites", headers: {'api_key': login_api_key, 'location': favorite_2.location}
+
+    expect(response.status).to eq(400)
   end
 end
