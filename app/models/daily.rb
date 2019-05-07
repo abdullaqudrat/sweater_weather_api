8a3907f31f276cacf58aae0fad415ff724e24ab0
abdullaqudrat <aqudrat630@gmail.com>
2019-05-06 19:33:09 -0600

    Adds time calc to models
    -Adds argument and calculation to daily and hourly models to give correct date and time to queried city

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,8 +1,8 @@
 class Daily
 
-  def initialize(daily_weather)
+  def initialize(daily_weather, time_difference)
     daily = daily_weather
-    @day = Time.at(daily[:time]).strftime('%A') #epoch time
+    @day = Time.at(daily[:time] - time_difference).strftime('%A') #epoch time
     @icon = daily[:icon] #description
     @summary = daily[:summary]
     @high_temp = daily[:temperatureHigh] #farenheit
--- app/models/hourly.rb
+++ app/models/hourly.rb
@@ -1,8 +1,8 @@
 class Hourly
 
-  def initialize(hourly_weather)
+  def initialize(hourly_weather, time_difference)
     hourly = hourly_weather
-    @hour = Time.at(hourly[:time]).strftime('%l %p') #epoch time
+    @hour = Time.at(hourly[:time] - time_difference).strftime('%l %p') #epoch time
     @icon = hourly[:icon] #description
     @summary = hourly[:summary]
     @temp = hourly[:temperature] #farenheit
